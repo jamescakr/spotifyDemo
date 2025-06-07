a74a37aa7e563e27d26b6fca01d8e3c2dd439e8f
jamesjo <jamescakr@gmail.com>
2025-06-06 23:54:02 -0600

    solved invalid issue so deleting debugging console.log code

--- src/common/components/LoginButton.tsx
+++ src/common/components/LoginButton.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@mui/material";
 import { getSpotifyAuthUrl } from "../../utils/auth";
 
-console.log("🔥 PROD CLIENT_ID →", import.meta.env.VITE_SPOTIFY_CLIENT_ID);
-console.log("🔥 PROD REDIRECT_URI →", import.meta.env.VITE_SPOTIFY_REDIRECT_URI);
-
 const LoginButton = () => {
   const login = () => {
     getSpotifyAuthUrl();
