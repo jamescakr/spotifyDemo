64af170c36a86bf6dd8c0729345c4cfc78c8656c
jamesjo <jamescakr@gmail.com>
2025-06-07 01:06:06 -0600

    add profile

--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,11 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, Avatar, IconButton } from "@mui/material";
 import LoginButton from "../../common/components/LoginButton";
+import useGetCurrentUserProfile from "../../hooks/useGetCurrentUserProfile";
 
 const Navbar = () => {
+  const { data: userProfile } = useGetCurrentUserProfile();
+
   return (
-    <Box display="flex" justifyContent="flex-end" alignItems="center" height="64px">
-      <LoginButton />
+    <Box
+      display="flex"
+      justifyContent="flex-end"
+      alignItems="center"
+      height="64px"
+      px={2}
+    >
+      {userProfile ? (
+        <IconButton size="large">
+          <Avatar
+            src={userProfile.images[0]?.url}
+            alt={userProfile.display_name ?? "User"}
+          />
+        </IconButton>
+      ) : (
+        <LoginButton />
+      )}
     </Box>
   );
 };
+
 export default Navbar;
